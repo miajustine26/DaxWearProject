e21b1f5b2dcae5d1bd93f6ad94765b0fa669749f
miajustine26 <mjgeneroso267@gmail.com>
2020-05-07 21:29:45 -0400

    Create routes

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def landing():
+    return render_template('landing.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
